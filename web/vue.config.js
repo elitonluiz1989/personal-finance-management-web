69228b77a0c18688701aec5a86be449f7558909b
elitonluiz1989 <elitonluiz1989@gmail.com>
2023-01-12 01:25:52 -0400

    Added web project

---
+++ web/vue.config.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const { defineConfig } = require("@vue/cli-service");
+
+const baseFolder =
+  process.env.APPDATA !== undefined && process.env.APPDATA !== ""
+    ? `${process.env.APPDATA}/ASP.NET/https`
+    : `${process.env.HOME}/.aspnet/https`;
+
+const certificateArg = process.argv
+  .map((arg) => arg.match(/--name=(?<value>.+)/i))
+  .filter(Boolean)[0];
+const certificateName = certificateArg
+  ? certificateArg.groups.value
+  : "PersonalFinanceManagement.Web";
+
+if (!certificateName) {
+  console.error(
+    "Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
+  );
+  process.exit(-1);
+}
+
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
+    },
+    port: 8080,
+  },
+});
